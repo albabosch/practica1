821bded535098843d0710d88ab6651905cb2adae
Alba Bosch Freire <albaboschfreire@MacBook-Air-de-Llucia.local>
2022-02-14 12:56:35 +0100

    llista

---
+++ llistabuida.py
@@ -0,0 +1,12 @@
+llista = []
+
+valor_1 = input("Indica valor 1: ")
+valor_2 = input("Indica valor 2: ")
+valor_3 = input("Indica valor 3: ")
+
+llista.append(valor_1)
+llista.append(valor_2)
+llista.append(valor_3)
+
+print(llista)
+
