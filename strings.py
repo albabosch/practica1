624b51920043bac5e04c11b5782beb96a37e71b2
Alba Bosch Freire <albaboschfreire@MacBook-Air-de-Llucia.local>
2022-02-23 08:56:40 +0100

    strings, lower i upper

---
+++ strings.py
@@ -0,0 +1,9 @@
+#per pasar una variable a minuscules: var.lower
+#per pasar varaible tot en majuscules: var.upper
+
+password = input("Escriu contra: ")
+
+password_l = password.lower()
+password_u = password.upper()
+
+print (password_l, password_u)
