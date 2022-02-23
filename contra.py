ef87d20a177e5488d7106177b74c53114cd875b9
Alba Bosch Freire <albaboschfreire@MacBook-Air-de-Llucia.local>
2022-02-23 09:04:03 +0100

    comprara contras

---
+++ contra.py
@@ -0,0 +1,12 @@
+#contrasenya donada, preguntar contrasenya i comprarar
+
+contrasenya = "hola"
+
+contra_usuari = input ("Escriu la contrasenya: ")
+
+contra_usuari = contra_usuari.lower()
+
+if contra_usuari == contrasenya:
+    print ("Coincideixen")
+else:
+    print("Diferents")
